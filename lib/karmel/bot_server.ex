b8df8075aada4ed1153042f9c343a30b487ae9fd
Maciej Woźniak <iskeld@gmail.com>
2018-03-27 20:44:02 +0200

    Update api

--- lib/karmel/bot_server.ex
+++ lib/karmel/bot_server.ex
@@ -3,35 +3,55 @@ defmodule Karmel.BotServer do
   alias Karmel.Request
 
   @bot Application.get_env(:karmel, :bot)
+  @slack_api Application.get_env(:karmel, :slack_api)
 
-  @spec dispatch_request(Karmel.Request.t()) :: {:ok, pid()} | :error
+  @spec dispatch_request(Karmel.Request.t()) :: {:ok, pid()} | {:error, String.t()}
   def dispatch_request(req = %Request{team_id: team_id}) do
-    bot_pid = case Karmel.BotRegistry.lookup(team_id) do
-      {:ok, pid} -> pid
-      :not_found -> spawn_bot(team_id)
-    end
+    bot_result =
+      case Karmel.BotRegistry.lookup(team_id) do
+        {:ok, pid} -> {:ok, pid}
+        :not_found -> spawn_bot(team_id)
+      end
 
-    if bot_pid != nil do
-      @bot.handle(bot_pid, req)
-      {:ok, bot_pid}
-    else
-      :error
+    case bot_result do
+      {:ok, pid} ->
+        @bot.handle(pid, req)
+        {:ok, pid}
+
+      {:error, msg} -> bot_result
     end
   end
 
+  @spec spawn_bot(String.t()) :: {:ok, pid()} | {:error, String.t()}
   def spawn_bot(team_id) do
-    team = Karmel.Team.get_by_team_id(team_id, true)
-    if team != nil do
+    with {:ok, team} <- load_team(team_id),
+         {:ok, slack_data} <- @slack_api.auth_test(team.token) do
       case Karmel.BotsSupervisor.start_child({@bot, team}) do
-        {:ok, pid} -> pid
-        {:error, {:already_started, pid}} -> pid
-        err -> 
-          Logger.error("Cannot start team #{team_id}. Error: #{inspect(err)}")
-          nil
+        {:ok, pid} ->
+          {:ok, pid}
+
+        {:error, {:already_started, pid}} ->
+          {:ok, pid}
+
+        err ->
+          {:error, "Error in spawn_bot for team #{team_id}: #{inspect(err)}"}
       end
     else
-      Logger.warn("Team #{team_id} does not exist")
-      nil
+      {:error, :not_found} ->
+        {:error, "Team #{team_id} does not exist"}
+
+      {:error, msg} ->
+        {:error, "Cannot retrieve team (#{team_id}) data: #{msg}"}
+
+      err ->
+        {:error, "Error in spawn_bot for team #{team_id}: #{inspect(err)}"}
+    end
+  end
+
+  defp load_team(team_id) do
+    case Karmel.Team.get_by_team_id(team_id, true) do
+      nil -> {:error, :not_found}
+      team -> {:ok, team}
     end
   end
 end
--- lib/karmel/slack/api.ex
+++ lib/karmel/slack/api.ex
@@ -1,3 +1,12 @@
 defmodule Karmel.Slack.Api do
-  @callback auth_test(token :: String.t()) :: {:ok, map()} | {:error, String.t()}
+  @callback auth_test(token :: String.t()) ::
+              {:ok,
+               %{
+                 team: String.t(),
+                 team_id: String.t(),
+                 url: String.t(),
+                 user: String.t(),
+                 user_id: String.t()
+               }}
+              | {:error, String.t()}
 end
